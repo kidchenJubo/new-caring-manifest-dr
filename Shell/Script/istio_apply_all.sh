08163c2b1221a3780a9f23e234fb1ac9c2c03aeb
ianchang <ianchang@jubo.health>
2025-09-11 12:21:13 +0800

    no message

--- Shell/Script/istio_apply_all.sh
+++ Shell/Script/istio_apply_all.sh
@@ -4,11 +4,11 @@
 
 # clear && /Users/ianchang/Work/new-caring/manifest/Shell/Script/istio_apply_all.sh
 
-gcloud config set project "jubo-care-platform"
+gcloud config set project "static-map-242406"
 
-gcloud auth application-default set-quota-project "jubo-care-platform"
+gcloud auth application-default set-quota-project "static-map-242406"
 
-gcloud container clusters get-credentials jcp-tw --region=asia-east1
+gcloud container clusters get-credentials caring-tw --region=asia-east1
 
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/authorization/policy/external/authorization-policy-external-allow-private-ips-demo.yaml
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/authorization/policy/external/authorization-policy-external-allow-private-ips-dev.yaml
@@ -45,6 +45,7 @@ kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/t
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/gateway/internal/infra-gateway-internal-qat.yaml
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/gateway/internal/infra-gateway-internal-release.yaml
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/gateway/neg/infra-gateway-neg-dev.yaml
+kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/gateway/neg/infra-gateway-neg-release.yaml
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/service/entry/googleapi.yaml
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/virtual/service/Infrastructure/argocd/argocd.yaml
 kubectl apply -f /Users/ianchang/Work/new-caring/manifest/Infrastructure/istio/templates/virtual/service/Microservices/Caring/Backend/Api/Internal/new-caring-web-api-internal-demo.yaml
